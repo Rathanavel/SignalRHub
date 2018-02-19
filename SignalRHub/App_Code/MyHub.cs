1b6bcfb517021a69de1feace21742080342bcd01
Rathanavel <Rathanavel@github.com>
2018-02-19 12:35:25 +0530

    Add project files.

---
+++ SignalRHub/App_Code/MyHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+public class MyHub : Hub
+{
+    public void Send(string message)
+    {
+        var id = Context.ConnectionId.Substring(0, 5);
+        Clients.All.receive(id, message);
+    }
+}
---
+++ SignalRHub/App_Code/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        app.MapSignalR();
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+    }
+}
